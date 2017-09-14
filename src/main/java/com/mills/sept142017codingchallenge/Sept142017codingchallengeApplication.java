f4a3847d17dff411cc6af80994c071e60619bb3c
millsmcilroy <millsmcilroy@gmail.com>
2017-09-14 12:16:45 -0600

    initial commit

---
+++ src/main/java/com/mills/sept142017codingchallenge/Sept142017codingchallengeApplication.java
@@ -0,0 +1,12 @@
+package com.mills.sept142017codingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sept142017codingchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sept142017codingchallengeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mills/sept142017codingchallenge/Sept142017codingchallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mills.sept142017codingchallenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sept142017codingchallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
